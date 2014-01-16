3049f5bad1051c2d41d7e8081e007d75e8102282
Peter Saxton <peterhsaxton@gmail.com>
2014-01-16 14:29:01 +0000

    Solved testcase for 1

---
+++ fizzbuzz.rb
@@ -0,0 +1,3 @@
+def fizzbuzz n
+	"FizzBuzz"
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,6 +6,10 @@ describe "Fizzbuzz " do
 	it "should return FizzBuzz for 0" do 
 		expect(fizzbuzz(0)).to eq("FizzBuzz")
 	end
+
+	it "should return 1 for 1" do
+		expect(fizzbuzz(1)).to eq(1)
+	end
 end
 
 
